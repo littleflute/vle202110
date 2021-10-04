a2e03e0edaa6c3de7d8b33bb8e94c196c418d71f
littleflute <littleflute@163.com>
2021-10-04 12:20:46 +0800

    player.html

---
+++ js/blApp.js
@@ -0,0 +1,13 @@
+var s = "<div id = 'id_div_4_blApp' >";
+s += "v0.3. 2 ";
+s += "</div>";
+
+var _g_ver_blApp = s;
+ 
+function _loadIssue6(o) {	
+	eval(o.body); 
+}
+w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/6", _loadIssue6);	
+
+
+ 
